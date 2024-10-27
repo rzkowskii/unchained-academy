fc7ce4aac2da300b48484d1af500b06171201419
Richard Ziolkowski <rzkowski@gmail.com>
2024-10-28 10:24:14 +1100

    fix: revert to simpler configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom';
 import { Suspense, lazy, useState, useEffect, ReactNode } from 'react';
 import { Loader2 } from 'lucide-react';
 import Layout from './components/layout/Layout';
